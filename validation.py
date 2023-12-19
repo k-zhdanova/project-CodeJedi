53a51c4a0e811afb793cb87a0b7db28b47296649
jane <janedeli81@gmail.com>
2023-12-19 16:14:21 +0100

    9-10 add mail and phone validation

--- validation.py
+++ validation.py
@@ -1,2 +1,11 @@
+import re
+
+
 class Validator:
-    pass
+    def validate_email(self, email):
+        return re.match(r"[^@]+@[^@]+\.[^@]+", email)
+
+    def validate_phone(self, phone):
+        return re.match(r"\+?1?\d{9,15}$", phone)
+
+
