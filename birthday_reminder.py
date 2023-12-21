30432f5fb634e539f98e292f3e24e106d5d3374d
KomrakovaAnna <komrakova.anna@gmail.com>
2023-12-21 09:20:38 +0000

    test

--- birthday_reminder.py
+++ birthday_reminder.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
+
 class BirthdayReminder:
     def get_upcoming_birthdays_contacts(self, contacts, days=30):
         upcoming = []
         current_date = datetime.now()
         for contact in contacts:
-            birthday = datetime.strptime(contact.birthday, '%Y-%m-%d')
+            birthday = datetime.strptime(contact.birthday, "%Y-%m-%d")
             if 0 <= (birthday - current_date).days <= days:
                 upcoming.append(contact)
         return upcoming
--- validation.py
+++ validation.py
@@ -6,6 +6,13 @@ def validate_email(email):
     return re.match(r"[^@]+@[^@]+\.[^@]+", email) is not None
 
 
+
+
 def validate_phone(phone):
     return re.match(r"\+?1?\d{9,15}$", phone) is not None
-#test
+
+
+
+
+
+
